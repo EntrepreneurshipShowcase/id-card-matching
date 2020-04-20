18885a4a2eaef4a8dcce967525e9737864682035
Tanish Baranwal <b.tanish@tugun.ai>
2020-04-20 14:47:59 -0700

    Adding third party facial recognition

--- id_database_fr.py
+++ id_database_fr.py
@@ -3,17 +3,14 @@ import numpy as np
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+
+import face_recognition
 import cv2
-import functools
-
-import predict
-
 class IDDatabase:
-    def __init__(self, predictor):
+    def __init__(self):
         self.cred = credentials.Certificate("./id-matching-fbebc4da731a.json")
         firebase_admin.initialize_app(self.cred)
         self.db = firestore.client()
-        self.predictor = predictor
         self.doc = self.db.collection(u"base").document(u"dhs")
     def add_person(self, name, id, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -21,45 +18,48 @@ class IDDatabase:
             str(id): {
                 u"name": name,
                 u"id": id,
-                u"vector": self.predictor.get_vec(image).tolist()[0],
+                u"vector": face_recognition.face_encodings(image)[0].tolist(),
             }
         }
         self.doc.update(data)
 
-    def get_ids(self):
+    def get_ids(self): 
         return self.doc.get()
+
 
 
 class DataDriver:
     def __init__(self):
-        self.predictor = predict.Predictor()
-        self.database = IDDatabase(self.predictor)
-        # Immediately get all ids
-        self.database.doc.on_snapshot(self.on_update)
+        self.database = IDDatabase()
         self.threshold = 95
+        self.database.doc.on_snapshot(self.on_update)
+
     def on_update(self, doc_snapshot, changes, read_time):
         for doc in doc_snapshot:
             self.id_vectors = doc.to_dict()
 
     def lookup_person(self, comp_image):
         comp_image = cv2.cvtColor(comp_image, cv2.COLOR_BGR2RGB)
-        comp_image_vec = self.predictor.get_vec(comp_image)[0]
+        comp_image_vec = face_recognition.face_encodings(comp_image)
+        if len(comp_image_vec) > 0:
+            comp_image_vec = comp_image_vec[0]
+        else:
+            return False, False
         min_dis = float("inf")
         matched_id = None
+        # import ipdb; ipdb.set_trace()
         for i in self.id_vectors.keys():
-            dis = self.predictor.get_distance(
-                comp_image_vec, np.array(self.id_vectors[i]["vector"])
-            )
+            dis = face_recognition.face_distance(np.array([self.id_vectors[i]["vector"]]), comp_image_vec)
             if dis < min_dis:
                 matched_id = self.id_vectors[i]["id"], self.id_vectors[i]["name"]
                 min_dis = dis
         return matched_id
-
-    def verify(self, id, comp_image, debug=False):
+    def verify_id(self, id, comp_image, debug=False):
         comp_image = cv2.cvtColor(comp_image, cv2.COLOR_BGR2RGB)
-        comp_image_vec = self.predictor.get_vec(comp_image)[0]
-        dis = self.predictor.get_distance(
-                comp_image_vec, np.array(self.id_vectors[str(id)]["vector"])
+        comp_image_vec = face_recognition.face_encodings(comp_image)[0]
+        id_vec = self.id_vectors[str(id)]["vector"]
+        dis = face_recognition.face_distance(
+                np.array([id_vec]), comp_image_vec
         )
         if debug:
             print(dis)
--- web_cam_test.py
+++ web_cam_test.py
@@ -1,22 +1,20 @@
 import cv2
 import numpy as np
 
-import predict
-import id_database
+import id_database_fr as db
 import face_crop
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # set Width
 cap.set(4, 640)  # set Height
-driver = id_database.DataDriver()
-cropper = face_crop.FaceCrop()
+driver = db.DataDriver()
 
-driver.add_person("Rohil", 1, cv2.imread("./ids/test_rohil.jpg"))
-driver.add_person("Tanish", 2, cv2.imread("./ids/test_tanish.jpg"))
-driver.add_person("Ayush", 3, cv2.imread("./ids/test_ayush.jpg"))
-driver.add_person("Sai", 4, cv2.imread("./ids/test_sai.jpg"))
-driver.add_person("Megna", 5, cv2.imread("./ids/test_megna.jpg"))
-driver.add_person("Jayani", 6, cv2.imread("./ids/test_jayani.jpg"))
+# driver.add_person("Rohil", 1, cv2.imread("./ids/test_rohil.jpg"))
+# driver.add_person("Tanish", 2, cv2.imread("./ids/test_tanish.jpg"))
+# driver.add_person("Ayush", 3, cv2.imread("./ids/test_ayush.jpg"))
+# driver.add_person("Sai", 4, cv2.imread("./ids/test_sai.jpg"))
+# driver.add_person("Megna", 5, cv2.imread("./ids/test_megna.jpg"))
+# driver.add_person("Jayani", 6, cv2.imread("./ids/test_jayani.jpg"))
 
 if __name__ == "__main__":
     while True:
@@ -25,8 +23,9 @@ if __name__ == "__main__":
 
         id, name = driver.lookup_person(frame)
         # id, name = driver.lookup_person(cropper.crop(frame))
+        font = cv2.FONT_HERSHEY_DUPLEX
+        frame = cv2.putText(frame, str(name), (10 + 6, 50), font, 1.0, (255, 255, 255), 1)
         cv2.imshow("web cam", frame)
-        print(name)
         k = cv2.waitKey(30) & 0xFF
         if k == 27:  # press 'ESC' to quit
             break
