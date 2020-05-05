575e7f73a7c407b8da0f5ce133c08348ae9d39fe
Tanish Baranwal <b.tanish@tugun.ai>
2020-05-04 18:10:44 -0700

    minor change

--- id_card_matching/convert_tflite.py
+++ id_card_matching/convert_tflite.py
@@ -2,11 +2,14 @@ import tensorflow as tf
 
 from dl.fpn_model import get_siamese_model
 from training_tools.validation import val_dataset
+import os
 
 def data_gen():
-    for i in val_dataset.take(2):
-        for j in i[0][0]:
-            yield([[j]])
+    for i in os.listdir("../temp_images"):
+        img = cv2.imread(i)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = (img - 127.5)/127.5
+        yield [[img]]
 
 model = get_siamese_model(training=False)
 # model = tf.keras.applications.ResNet50(include_top=False, input_shape=(96, 96, 3))
