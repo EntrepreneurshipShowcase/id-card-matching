cbae2fe555d3c79078b26074112becc35d810f7d
Tanish Baranwal <b.tanish@tugun.ai>
2020-05-04 18:39:14 -0700

    fixing stuff

--- id_card_matching/dl/fpn_model.py
+++ id_card_matching/dl/fpn_model.py
@@ -64,6 +64,7 @@ class TripletAccuracy(layers.Layer):
 class Siamese(layers.Layer):
     def __init__(self, input_shape):
         super(Siamese, self).__init__()
+        self.global_pool = layers.GlobalMaxPool2D()
         self.embedding_p3 = Embedding()
         self.embedding_p4 = Embedding()
         self.embedding_p5 = Embedding()
@@ -100,66 +101,9 @@ class Siamese(layers.Layer):
 
         return p2_a, p3_a, p4_a, p5_a, p6_a
 
-
 def get_siamese_model(training=True):
     inp_shape = (96, 96, 3)
-    base_model = resnet.ResNet50(include_top=False, input_shape=inp_shape, weights="imagenet")
-    backbone = tf.keras.Model(inputs=base_model.input, outputs=(base_model.get_layer("conv2_block3_out").output, base_model.get_layer("conv3_block4_out").output, base_model.get_layer("conv4_block6_out").output, base_model.output))
-
-    embedding_p3 = tf.keras.Sequential([layers.Conv2D(EMBEDDING_LAYER_DIM*2, (2, 2)), layers.BatchNormalization(), layers.Activation("relu"), layers.Conv2D(EMBEDDING_LAYER_DIM, (1, 1)), layers.BatchNormalization(), layers.Activation("relu"), layers.GlobalAveragePooling2D()])
-    embedding_p4 = tf.keras.Sequential([layers.Conv2D(EMBEDDING_LAYER_DIM*2, (2, 2)), layers.BatchNormalization(), layers.Activation("relu"), layers.Conv2D(EMBEDDING_LAYER_DIM, (1, 1)), layers.BatchNormalization(), layers.Activation("relu"), layers.GlobalAveragePooling2D()])
-    embedding_p5 = tf.keras.Sequential([layers.Conv2D(EMBEDDING_LAYER_DIM*2, (2, 2)), layers.BatchNormalization(), layers.Activation("relu"), layers.Conv2D(EMBEDDING_LAYER_DIM, (1, 1)), layers.BatchNormalization(), layers.Activation("relu"), layers.GlobalAveragePooling2D()])
-    embedding_p2 = tf.keras.Sequential([layers.Conv2D(EMBEDDING_LAYER_DIM*2, (2, 2)), layers.BatchNormalization(), layers.Activation("relu"), layers.Conv2D(EMBEDDING_LAYER_DIM, (1, 1)), layers.BatchNormalization(), layers.Activation("relu"), layers.GlobalAveragePooling2D()])
-    embedding_p6 = tf.keras.Sequential([layers.Conv2D(EMBEDDING_LAYER_DIM*2, (2, 2)), layers.BatchNormalization(), layers.Activation("relu"), layers.Conv2D(EMBEDDING_LAYER_DIM, (1, 1)), layers.BatchNormalization(), layers.Activation("relu"), layers.GlobalAveragePooling2D()])
-    # backbone = resnet50.ResNet(101)
-    # backbone.load_weights("./faster_rcnn.h5", by_name=True)
-    # backbone.trainable = False
-
-    input_c2 = layers.Input((24, 24, 256))
-    input_c3 = layers.Input((12, 12, 512))
-    input_c4 = layers.Input((6, 6, 1024))
-    input_c5 = layers.Input((3, 3, 2048))
-
-    out_channels = 256
-        
-    fpn_c2p2 = layers.Conv2D(out_channels, (1, 1), 
-                                    kernel_initializer='he_normal', name='fpn_c2p2')
-    fpn_c3p3 = layers.Conv2D(out_channels, (1, 1), 
-                                    kernel_initializer='he_normal', name='fpn_c3p3')
-    fpn_c4p4 = layers.Conv2D(out_channels, (1, 1), 
-                                    kernel_initializer='he_normal', name='fpn_c4p4')
-    fpn_c5p5 = layers.Conv2D(out_channels, (1, 1), 
-                                    kernel_initializer='he_normal', name='fpn_c5p5')
-    
-    fpn_p3upsampled = layers.UpSampling2D(size=(2, 2), name='fpn_p3upsampled')
-    fpn_p4upsampled = layers.UpSampling2D(size=(2, 2), name='fpn_p4upsampled')
-    fpn_p5upsampled = layers.UpSampling2D(size=(2, 2), name='fpn_p5upsampled')
-    
-    
-    fpn_p2 = layers.Conv2D(out_channels, (3, 3), padding='SAME', 
-                                kernel_initializer='he_normal', name='fpn_p2')
-    fpn_p3 = layers.Conv2D(out_channels, (3, 3), padding='SAME', 
-                                kernel_initializer='he_normal', name='fpn_p3')
-    fpn_p4 = layers.Conv2D(out_channels, (3, 3), padding='SAME', 
-                                kernel_initializer='he_normal', name='fpn_p4')
-    fpn_p5 = layers.Conv2D(out_channels, (3, 3), padding='SAME', 
-                                kernel_initializer='he_normal', name='fpn_p5')
-    
-    fpn_p6 = layers.MaxPooling2D(pool_size=(1, 1), strides=2, name='fpn_p6')
-
-
-    P5 = fpn_c5p5(input_c5)
-    P4 = fpn_c4p4(input_c4) + fpn_p5upsampled(P5)
-    P3 = fpn_c3p3(input_c3) + fpn_p4upsampled(P4)
-    P2 = fpn_c2p2(input_c2) + fpn_p3upsampled(P3)
-    
-    # Attach 3x3 conv to all P layers to get the final feature maps.
-    P2 = fpn_p2(P2)
-    P3 = fpn_p3(P3)
-    P4 = fpn_p4(P4)
-    P5 = fpn_p5(P5)
-    P6 = fpn_p6(P5)
-    neck = tf.keras.Model(inputs=[input_c2, input_c3, input_c4, input_c5], outputs=[P2, P3, P4, P5, P6])
+    siamese = Siamese(inp_shape)
     if training:
         input_a = layers.Input(inp_shape, name="anchor")
         input_p = layers.Input(inp_shape, name="positive")
@@ -174,57 +118,9 @@ def get_siamese_model(training=True):
         #     ),
         # )
 
-        C2_a, C3_a, C4_a, C5_a = backbone(input_a, training=training)
-        P2_a, P3_a, P4_a, P5_a, P6_a = neck([C2_a, C3_a, C4_a, C5_a], training=training)
-
-        p2_a = embedding_p2(P2_a)
-
-        p3_a = embedding_p3(P3_a)
-        p3_a = layers.Add()([p2_a, p3_a])
-
-        p4_a = embedding_p4(P4_a)
-        p4_a = layers.Add()([p3_a, p4_a])
-
-        p5_a = embedding_p5(P5_a)
-        p5_a = layers.Add()([p4_a, p5_a])
-
-        p6_a = embedding_p6(P6_a)
-        p6_a = layers.Add()([p5_a, p6_a])
-
-
-        C2_p, C3_p, C4_p, C5_p = backbone(input_p, training=training)
-        P2_p, P3_p, P4_p, P5_p, P6_p = neck([C2_p, C3_p, C4_p, C5_p], training=training)
-
-        p2_p = embedding_p2(P2_p)
-
-        p3_p = embedding_p3(P3_p)
-        p3_p = layers.Add()([p2_p, p3_p])
-
-        p4_p = embedding_p4(P4_p)
-        p4_p = layers.Add()([p3_p, p4_p])
-
-        p5_p = embedding_p5(P5_p)
-        p5_p = layers.Add()([p4_p, p5_p])
-
-        p6_p = embedding_p6(P6_p)
-        p6_p = layers.Add()([p5_p, p6_p])
-
-        C2_n, C3_n, C4_n, C5_n = backbone(input_n, training=training)
-        P2_n, P3_n, P4_n, P5_n, P6_n = neck([C2_n, C3_n, C4_n, C5_n], training=training)
-
-        p2_n = embedding_p2(P2_n)
-
-        p3_n = embedding_p3(P3_n)
-        p3_n = layers.Add()([p2_n, p3_n])
-
-        p4_n = embedding_p4(P4_n)
-        p4_n = layers.Add()([p3_n, p4_n])
-
-        p5_n = embedding_p5(P5_n)
-        p5_n = layers.Add()([p4_n, p5_n])
-
-        p6_n = embedding_p6(P6_n)
-        p6_n = layers.Add()([p5_n, p6_n])
+        p2_a, p3_a, p4_a, p5_a, p6_a = siamese(input_a, training=training)
+        p2_p, p3_p, p4_p, p5_p, p6_p = siamese(input_p, training=training)
+        p2_n, p3_n, p4_n, p5_n, p6_n = siamese(input_n, training=training)
         
         p2_a, p2_p, p2_n = TripletLoss(margin=4)(p2_a, p2_p, p2_n)
         p3_a, p3_p, p3_n = TripletLoss(margin=7)(p3_a, p3_p, p3_n)
@@ -241,23 +137,8 @@ def get_siamese_model(training=True):
             inputs=[input_a, input_p, input_n], outputs=p6
         )
     else:
-        input_x = layers.Input(inp_shape)
-        C2_x, C3_x, C4_x, C5_x = backbone(input_x, training=training)
-        P2_x, P3_x, P4_x, P5_x, P6_x = neck([C2_x, C3_x, C4_x, C5_x], training=training)
-
-        p2_x = embedding_p2(P2_x)
-
-        p3_x = embedding_p3(P3_x)
-        p3_x = layers.Add()([p2_x, p3_x])
-
-        p4_x = embedding_p4(P4_x)
-        p4_x = layers.Add()([p3_x, p4_x])
-
-        p5_x = embedding_p5(P5_x)
-        p5_x = layers.Add()([p4_x, p5_x])
-
-        p6_x = embedding_p6(P6_x)
-        p6_x = layers.Add()([p5_x, p6_x])
+        input_x = layers.Input(inp_shape, name="input")
+        _, _, _, _, p6_x = siamese(input_x, training=training)
         model = tf.keras.models.Model(inputs=input_x, outputs=p6_x)
 
     return model
