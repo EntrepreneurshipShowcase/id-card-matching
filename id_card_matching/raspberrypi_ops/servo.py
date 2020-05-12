aca7977813c09f04751530f6df26b72b309f5e5e
Tanish <b.tanish@gmail.com>
2020-05-12 01:20:09 +0100

    Changing servo angle

--- id_card_matching/raspberrypi_ops/servo.py
+++ id_card_matching/raspberrypi_ops/servo.py
@@ -1,7 +1,7 @@
 import gpiozero as gz
 
 class Servo:
-    def __init__(self, servo_pin, max_angle=45):
+    def __init__(self, servo_pin, max_angle=25):
         self.max_angle = max_angle
         self.servo = gz.Servo(servo_pin, max_pulse_width=(1450+10.6*max_angle)/(1e6), min_pulse_width=(1450-10.6*max_angle)/(1e6))
     def mid(self):
